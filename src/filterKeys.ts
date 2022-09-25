70c1ac6131d5864c5814214a4a474f63ce305715
Alex Grozav <alex@grozav.com>
2022-09-25 15:04:00 +0300

    fix: improve filterKeys performance

--- src/filterKeys.ts
+++ src/filterKeys.ts
@@ -5,34 +5,33 @@
 export function filterKeys<T = Record<string, any>> (source: T, options: { allowlist?: Array<string | RegExp>; denylist?: Array<string | RegExp>; }): Partial<T> {
     const { allowlist, denylist } = options;
 
-    return Object.keys(source)
-        .filter(key => {
-            let matches = true;
+    return Object.keys(source).reduce<Partial<T>>((acc, key: string) => {
+        let matches = true;
 
-            if (allowlist?.length) {
-                matches = matches && allowlist.some((pattern) => {
-                    if (typeof pattern === 'string') {
-                        return key === pattern;
-                    }
+        if (allowlist?.length) {
+            matches = matches && allowlist.some((pattern) => {
+                if (typeof pattern === 'string') {
+                    return key === pattern;
+                }
 
-                    return key.match(pattern);
-                });
-            }
+                return key.match(pattern);
+            });
+        }
 
-            if (denylist?.length) {
-                matches = matches && !denylist.some((pattern) => {
-                    if (typeof pattern === 'string') {
-                        return key === pattern;
-                    }
+        if (denylist?.length) {
+            matches = matches && !denylist.some((pattern) => {
+                if (typeof pattern === 'string') {
+                    return key === pattern;
+                }
 
-                    return key.match(pattern);
-                });
-            }
+                return key.match(pattern);
+            });
+        }
 
-            return matches;
-        })
-        .reduce<Partial<T>>((acc, key: string) => {
+        if (matches) {
             (acc as Record<string, any>)[key] = (source as Record<string, any>)[key];
-            return acc;
-        }, {});
+        }
+
+        return acc;
+    }, {});
 }
